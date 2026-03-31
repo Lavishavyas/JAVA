b24af91f7df460568d4f6577fb39562192f1e2e9
Lavisha Vyas <165463124+Lavishavyas@users.noreply.github.com>
2026-03-31 16:01:40 +0530

    Add files via upload

---
+++ Hey.java
@@ -0,0 +1,18 @@
+class Hey {
+    public static void main(String[] args) {
+        
+        int num1 = 7;
+        // int num2 = 5;
+
+        // int result = num1 % num2;
+        // num1 = num1 + 2;
+        // num1++; // post - increment
+        // num1--;
+        // ++num1; // pre - increment
+
+        int result = num1++;
+
+
+        System.out.println(result);
+    }
+}
