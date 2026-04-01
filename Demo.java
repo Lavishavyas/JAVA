020da558c9067ffc59f02d5bc2b7a629d6f054da
Lavisha Vyas <165463124+Lavishavyas@users.noreply.github.com>
2026-04-01 15:01:26 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main(String args[]) {
+        
+        double x = 8.8;
+        double y = 9.8;
+
+        boolean result = x >= y;
+
+        System.out.println(result);
+
+    }
+}
